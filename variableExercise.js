c826ccb3c46533e0fd0089b9cdf6840f1977b28b
zachmann00 <zach_mann@hotmail.com>
2021-02-23 13:27:20 -0500

    assignment finished

---
+++ variableExercise.js
@@ -0,0 +1,38 @@
+// 1
+// Constant Variable Type (const)
+// 2
+let num = 20;
+console.log(num);
+// 3
+num = 15;
+console.log(num);
+// 4
+num = num + 7;
+console.log(num);
+// 5
+num --;
+console.log(num);
+// 6
+num = "This is not a number";
+console.log(num);
+// 7
+let blank;
+// 8
+console.log(blank);
+//9
+const blank = null;
+console.log(blank);
+
+// BONUS
+//1 2 3 4
+const quarter = .25;
+const dime = .10;
+const nickel = .05;
+const penny = .01;
+
+var money = quarter + dime + nickel + penny
+console.log(money);
+
+let letters = "qwertyuiop";
+console.log(letters.length);
+console.log("qwertyuiop"[9]);
